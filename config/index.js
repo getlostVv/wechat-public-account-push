49468edf7311972f4dc0e46e1b376f0518d8204a
getlostVv <112672575+getlostVv@users.noreply.github.com>
2022-09-02 11:26:58 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -103,11 +103,11 @@ export const config = {
 
     }
 
-// {{date.DATA}}  
-// 城市：{{city.DATA}}  
-// 天气：{{weather.DATA}}  
-// 最低气温: {{min_temperature.DATA}}  
-// 最高气温: {{max_temperature.DATA}}  
+ {{date.DATA}}  
+ 城市：{{city.DATA}}  
+ 天气：{{weather.DATA}}  
+ 最低气温: {{min_temperature.DATA}}  
+ 最高气温: {{max_temperature.DATA}}  
 // 今天是我们恋爱的第{{love_day.DATA}}天
 // 今天是我们结婚的第{{marry_day.DATA}}天
 // {{birthday_message.DATA}}
